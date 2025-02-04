db1873b0d83e8b7a9c00470ff036968174636c3b
admin <rong@agile-solutions.ch>
2025-02-04 15:06:40 +0100

    fix tagging

--- src/main/java/com/agilesolutions/poc/config/MessageProperties.java
+++ src/main/java/com/agilesolutions/poc/config/MessageProperties.java
@@ -2,7 +2,9 @@ package com.agilesolutions.poc.config;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "config")
 @Data
 public class MessageProperties {
